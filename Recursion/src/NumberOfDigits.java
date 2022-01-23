3962e1977447dac37da82fc53e02aeedd2e9bed7
Vishu Goyal <goalsachiever8@gmail.com>
2022-01-23 13:40:04 +0530

    NumberOfDigits.java head and tail

---
+++ Recursion/src/NumberOfDigits.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class NumberOfDigits
+{
+    // Head Recursion
+//    public static int numberDigits(int n)
+//    {
+//        if(n==0)
+//        {
+//            return 0;
+//        }
+//        return 1+numberDigits(n/10);
+//    }
+
+    // Tail Recursion
+    public static void numberDigits(int n,int ans)
+    {
+        if(n==0)
+        {
+            System.out.println(ans);
+            return;
+        }
+
+        numberDigits(n/10,ans+1);
+    }
+}
+
+class main2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+//        System.out.println(NumberOfDigits.numberDigits(num));   // Head Recursion calling
+        NumberOfDigits.numberDigits(num,0);   // Head Recursion calling
+    }
+}
